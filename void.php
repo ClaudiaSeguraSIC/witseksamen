eebc10a53f8b8948817a71d6decc19e55d08631e
ClaudiaSeguraSIC <segura@ruc.dk>
2022-11-11 16:33:29 +0100

    Update void.php

--- void.php
+++ void.php
@@ -31,11 +31,13 @@
 
 					let enteringTheVoid; = "theVoid";
 
-					alert("Hej "+enteringTheVoid);
-
-					alert("hvad som helst");
+					alert("Hello"+enteringTheVoid);
 
 					</script>
+
+					<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1131831136&color=%233c24cc&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/claudia-segura-vestergaer" title="SīC[URA]" target="_blank" style="color: #cccccc; text-decoration: none;">SīC[URA]</a> · <a href="https://soundcloud.com/claudia-segura-vestergaer/707dolphins-2-mp3" title="707Dolphins 2.mp3" target="_blank" style="color: #cccccc; text-decoration: none;">707Dolphins 2.mp3</a></div>
+
+					<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/518222379&color=%23a83ba8&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/claudia-segura-vestergaer" title="SīC[URA]" target="_blank" style="color: #cccccc; text-decoration: none;">SīC[URA]</a> · <a href="https://soundcloud.com/claudia-segura-vestergaer/apollo-love-apollo11" title="Apollo Love (Apollo11)" target="_blank" style="color: #cccccc; text-decoration: none;">Apollo Love (Apollo11)</a></div>
 
 
 
